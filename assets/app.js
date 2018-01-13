0813c88ea4a40ee36cdbfe30c5c471f598039f71
Henry Mesias <henrymesias@gmail.com>
2018-01-13 11:06:44 -0500

    updated js to get data from omdb

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,22 @@
+$(document).ready(function() {
+
 //Henry's Work********************
 // constructing a URL to search OMDB
 var apiKey = 'ebd97e72';
-var searchTerm = '';
-var queryURL = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t' + searchTerm; 
+var searchTerm = 'batman';
+var queryURL = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t=' + searchTerm;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  // After the data comes back from the API
+  .done(function(response) {
+    console.log(response);
+   // Storing an array of results in the results variable
+   var results = response.data;
+
+  });
+
+});
