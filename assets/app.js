978f7b5e76de8f2758c6e343996d6a1656fc8a27
Henry Mesias <henrymesias@gmail.com>
2018-01-13 11:00:01 -0500

    added api key and initial variable

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+//Henry's Work********************
+// constructing a URL to search OMDB
+var apiKey = 'ebd97e72';
+var searchTerm = '';
+var queryURL = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t' + searchTerm; 
