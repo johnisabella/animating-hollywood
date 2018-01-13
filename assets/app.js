771e82fd8b8dc5a394ff7cb2a8f39818e74af7bc
John Isabella <john.isabella@gmail.com>
2018-01-13 13:25:33 -0500

    x

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,39 @@
-      // This .on("click") function will trigger the AJAX Call
-      $("#find-movie").on("click", function(event) {
+// This .on("click") function will trigger the AJAX Call
+$("#find-movie").on("click", function(event) {
 
-        // event.preventDefault() can be used to prevent an event's default behavior.
-        // Here, it prevents the submit button from trying to submit a form when clicked
-        event.preventDefault();
+  // event.preventDefault() can be used to prevent an event's default behavior.
+  // Here, it prevents the submit button from trying to submit a form when clicked
+  event.preventDefault();
 
-        // Here we grab the text from the user input box
-        var movie = $("#movie-input").val().trim();
+  // Here we grab the text from the user input box
+  var movie = $("#movie-input").val().trim();
 
-        // Here we construct our URL to be sent to the OMDB API
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  // Here we construct our URL to be sent to the OMDB API
+  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-          var actors = response.actors
-          $("#actors-view").text(actors);
-        });
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    var actors = response.actors
+  });
 
-//use a for loop to get each actor on a different line. attach a p element to it.
-        // -----------------------------------------------------------------------
+  //remove user input from the movie-input div class to allow users to enter additional movie titles.
+  $("#movie-input").empty();
 
-//GIT CHECK 1:06pm 1/13/18
-      });
+  // Looping through the array of actors from the OMDB API to display in the DOM
+  for (var i = 0; i < actors.length; i++) {
+
+    // Then dynamicaly generating buttons for each actor in the array.
+    var a = $("<button>");
+    // Adding a class
+    a.addClass("btn btn-info");
+    a.addClass("btnStyle");
+    // Adding a data-attribute with a value of the actor at index i
+    a.attr("data-name", actors[i]);
+    // Providing the button's text with a value of the actors at index i
+    a.text(actors[i]);
+    // Adding the button to the HTML
+    $("#actors-view").append(a);
+  }
+});
