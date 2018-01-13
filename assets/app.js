98ff2d384fb8eaf02a482c17a98113b46bf330e4
John Isabella <john.isabella@gmail.com>
2018-01-13 13:31:52 -0500

    ready for merge

--- assets/app.js
+++ assets/app.js
@@ -7,15 +7,24 @@ $("#find-movie").on("click", function(event) {
 
   // Here we grab the text from the user input box
   var movie = $("#movie-input").val().trim();
+  var apiKey = 'ebd97e72';
 
   // Here we construct our URL to be sent to the OMDB API
-  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  // Long plot query URL
+  var queryURL = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t=' + movie;
+
+  // Short plot query- delete later?
+  // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + apiKey;
 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function(response) {
-    var actors = response.actors
+  })
+  .done(function(response) {
+      console.log(response);
+    var actors = response.Actors;
+    console.log(actors);
+    $("#actors-view").text(actors);
   });
 
   //remove user input from the movie-input div class to allow users to enter additional movie titles.
